8b9ae3b640ed23723a9e49c628076fba76f4dfe2
Alice Li Maunumäki <aliceli@Alices-MacBook-Pro.local>
2025-10-02 11:18:40 +0300

    cube camera movement and mouse movement

--- 06-Cameras/src/script.js
+++ 06-Cameras/src/script.js
@@ -1,5 +1,21 @@
 import * as THREE from 'three'
 
+//different cameras: cube camera (environment), stereo camera (3D), orthographic (without perspective)
+//perspective camera
+
+/**
+ * Cursor
+ */
+
+const cursor = {
+	x: 0,
+	y: 0
+}
+window.addEventListener('mousemove', (event) => {
+	cursor.x = -(event.clientX / sizes.width - 0.5) //0 - 1 as much left as the right
+	cursor.y = event.clientY / sizes.height - 0.5
+})
+
 /**
  * Base
  */
@@ -23,12 +39,24 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100) //parameters(FOV vertical part), aspect ratio, near and far
+// how close and how far the camera can see between visible and not visible
+//don't use extreme values 0.0001 and 9999999 prevent z-fighting and glitching
+
+//orthographic camera
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100) //left right, top, bottom, near, far. parallel renders
+//square render (-1, 1, 1, 1) but render is not a square so you have to calculate the aspect ratio
+
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -41,10 +69,21 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    // const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+
+	//Update camera
+	// camera.position.x = cursor.x * 3 // this puts the camera in 1 position
+	// camera.position.y = cursor.y * 3
+	// camera.lookAt(mesh.position)
+
+	//get the camera to go all the way around the cube x & z
+	camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+	camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+	camera.position.y = cursor.y * 5
+	camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
