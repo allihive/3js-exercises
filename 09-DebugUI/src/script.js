1cf51d1a7add15649456175ab98d0515b739b900
Alice Li Maunumäki <aliceli@Alices-MacBook-Pro.local>
2025-10-09 11:33:34 +0300

    gui tweaks

--- 09-DebugUI/src/script.js
+++ 09-DebugUI/src/script.js
@@ -1,6 +1,33 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from  'lil-gui'
+
+/*
+Notes:
+debug UI:
+lil-gui library popular, well maintained and easy to use
+import * as dat from 'lil-gui'
+tweaks: range, color, text, checkbox, select, button
+only twek properties of objects
+*/
+
+/*Debug */
+const gui = new GUI({
+	width: 340,
+	title: 'Nice debug UI',
+	closeFolders: false
+})
+// gui.close()
+// gui.hide()
+
+window.addEventListener('keydown', (event) => {
+	if (event.key == 'h')
+	{
+		gui.show(gui._hidden)
+	}
+})
+const debugObject = {}
 
 /**
  * Base
@@ -14,10 +41,71 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+
+debugObject.color = '#3a6ea6'
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+const cubeTweaks = gui
+	.addFolder('Awesome')
+cubeTweaks.close()
+
+/*Range*/
+// gui.add(mesh.position, 'y', -3, 3, 0.01) //object, then the property, min, max, position
+cubeTweaks
+	.add(mesh.position, 'y')
+	.min(-3)
+	.max(3)
+	.step(0.01) //don't have to check the documentation
+	.name('elevation')
+
+//create an object whose purpose is to hold properties
+
+/*checkbox for debug UI booleans*/
+cubeTweaks
+	.add(mesh, 'visible')
+cubeTweaks
+	.add(material, 'wireframe')
+
+/*Colors*/
+cubeTweaks.addColor(debugObject, 'color')
+	.onChange(() => {
+		material.color.set(debugObject.color)
+	}) //applies color management not the same value used inside 3js
+//retrieve what three.js is using internally
+
+//animate the object button
+debugObject.spin = () => {
+	gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2})
+}
+
+cubeTweaks.add(debugObject, 'spin')
+
+/*Tweak geometry*/
+//make the wireframe true in the materials
+//width height and depth dimensions
+
+// cubeTweaks.add(geometry, 'widthsegments') //used to generate the whole geometry only once
+//need to add subdivision property to the debug object
+debugObject.subdivision = 2 // destroy old geometry and build a new one
+cubeTweaks
+	.add(debugObject, 'subdivision')
+	.min(1)
+	.max(20)
+	.step(1)
+	.onFinishChange(() => {
+		mesh.geometry.dispose()
+		mesh.geometry= new THREE.BoxGeometry(
+			1, 1, 1,
+			debugObject.subdivision, debugObject.subdivision, debugObject.subdivision)
+	})
+	//onFinishchange helps the CPU, when you release the click then it wil apply the change
+	//old geometries are sitting somewhere in the GPU memory -> memory leak
+
+
+
 
 /**
  * Sizes
